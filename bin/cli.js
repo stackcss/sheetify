72907ede3744ba23184e15258e3c372096db93b3
Yoshua Wuyts <i@yoshuawuyts.com>
2015-09-08 18:44:11 +1000

    bin: clean up cli.js
    Signed-off-by: Yoshua Wuyts <i@yoshuawuyts.com>

--- bin/cli.js
+++ bin/cli.js
@@ -1,71 +1,58 @@
 #!/usr/bin/env node
+const cliclopts = require('cliclopts')
+const minimist = require('minimist')
+const fs = require('fs')
 
-var subarg = require('subarg')
-var path = require('path')
-var fs = require('fs')
+const main = require('../')
 
-var sheetify = require('..')
+const opts = cliclopts([
+  { name: 'help', abbr: 'h', boolean: true },
+  { name: 'version', abbr: 'v', boolean: true },
+  { name: 'compress', abbr: 'c', boolean: true },
+  { name: 'debug', abbr: 'd', boolean: true },
+  { name: 'transform', abbr: 't' }
+])
 
-var argv = subarg(process.argv.slice(2))
-
-convertAliases(argv, {
-  t: 'transform',
-  c: 'compress',
-  v: 'version',
-  d: 'debug'
-}, {
-  transform: []
-})
-
-var files = argv._
-files = files.map(String)
-files = files.map(function (entry) {
+const argv = minimist(process.argv.slice(2), opts.options())
+const files = argv._.map(function (file) {
+  const entry = String(file)
   return /^\.\//g.test(entry) ? entry : './' + entry
 })
 
-if (!files.length) help()
+// parse options
+if (argv.version) {
+  const version = require('../package.json').version
+  process.stdout.write('v' + version)
+  process.exit(0)
+}
+else if (argv.help) usage(0)
+else if (!files.length) usage(1)
 else {
-  var bundler = sheetify(files[0])
+  const bundler = main(files[0])
 
-  argv.transform.forEach(function (tr) {
+  const transforms = toArray(argv.transform)
+  transforms.forEach(function (tr) {
     bundler.transform(tr)
   })
 
-  const opts = {
-    compress: argv.compress || false,
-    debug: argv.debug || false
-  }
-  bundler.bundle(opts, function (err, data) {
+  bundler.bundle(argv, function (err, data) {
     if (err) throw err
     process.stdout.write(data)
     process.stdout.write('\n')
   })
 }
 
-function help () {
-  var $0 = path.basename(process.argv[1])
-  var usage = fs.readFileSync(__dirname + '/usage.txt', 'utf8')
-  process.stdout.write(usage.replace(/\$0/g, $0))
+// print usage & exit
+// num? -> null
+function usage (exitCode) {
+  fs.createReadStream(__dirname + '/usage.txt')
+    .pipe(process.stdout)
+    .on('close', process.exit.bind(null, exitCode))
 }
 
-function convertAliases (argv, aliases, defaults) {
-  Object.keys(aliases).forEach(function (alias) {
-    var key = aliases[alias]
-
-    if (key in argv) argv[key] = makeArray(argv[key])
-    if (!(alias in argv)) return
-
-    var value = makeArray(argv[alias])
-    argv[key] = argv[key] ? value.concat(argv[key]) : value
-    delete argv[alias]
-  })
-
-  Object.keys(defaults).forEach(function (key) {
-    if (key in argv) return
-    argv[key] = defaults[key]
-  })
-}
-
-function makeArray (value) {
-  return Array.isArray(value) ? value : [value]
+// transform a value to an array
+// [any]|any -> [any]
+function toArray (val) {
+  if (!val) return []
+  return Array.isArray(val) ? val : [val]
 }
