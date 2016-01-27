de40f0b3bf35d61dec6dc7465d99f856a1babe75
Yoshua Wuyts <i@yoshuawuyts.com>
2016-01-28 10:34:19 +1100

    docs: annotate source

--- transform.js
+++ transform.js
@@ -1,13 +1,17 @@
 const staticModule = require('static-module')
-const stream = require('readable-stream')
-const sheetify = require('./')
-const path = require('path')
-const resolve = require('resolve')
-const pump = require('pump')
 const fromString = require('from2-string')
+const stream = require('readable-stream')
+const xtend = require('xtend/mutable')
+const resolve = require('resolve')
+const path = require('path')
+const pump = require('pump')
+
+const sheetify = require('./')
 
 module.exports = transform
 
+// inline sheetify transform for browserify
+// (str, opts) -> str
 function transform (filename, options) {
   const basedir = path.dirname(filename)
   const vars = {
@@ -17,19 +21,16 @@ function transform (filename, options) {
   }
 
   options = options || {}
-  if (options.vars) {
-    Object.keys(options.vars).forEach(function (key) {
-      vars[key] = options.vars[key]
-    })
-  }
+  if (options.vars) xtend(vars, options.vars)
 
-  const sm = staticModule(
-    { sheetify: staticSheetify },
-    { vars: vars, varModules: { path: path } }
-  )
+  const smOpts = { vars: vars, varModules: { path: path } }
+  const sm = staticModule({ sheetify: staticSheetify }, smOpts)
 
   return sm
 
+  // replace an inline sheetify require statement
+  // with a hash and attach css to top of the bundle
+  // (str, obj) -> rstream
   function staticSheetify (sheetFilename, sheetOptions) {
     sheetOptions = sheetOptions || {}
     sheetOptions.basedir = sheetOptions.basedir || basedir
@@ -45,6 +46,7 @@ function transform (filename, options) {
         JSON.stringify(uuid),
         ')'
       ].join(''))
+
       pump(sheetStream, pts)
       sm.emit('file', path.join(basedir, sheetFilename))
     })
@@ -52,6 +54,8 @@ function transform (filename, options) {
     return pts
   }
 
+  // resolve a path
+  // str -> str
   function resolver (p) {
     return resolve.sync(p, { basedir: path.dirname(filename) })
   }
